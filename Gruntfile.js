b21e0cc0b67043c9375d250b01608f4ca5ed4c32
digbyk <digby@digby.net>
2015-04-19 01:43:40 +0100

    Updated G+ link

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,46 +1,43 @@
 module.exports = function (grunt) {
 
-	grunt.initConfig({
-		jshint: {
-			files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js', 'js/**/*.js'],
-			options: {
-				globals: {
-					jQuery: true
-				}
-			}
-		},
-		watch: {
-			js: {
-				files: ['<%= jshint.files %>'],
-				tasks: ['jshint']
-		jshint: {
-			},
-			styles: {
-				files: ['less/**/*.less'], // which files to watch
-				tasks: ['less'],
-				options: {
-					nospawn: true
-				}
-			}
-		},
-		less: {
-			development: {
-				options: {
-					compress: true,
-					yuicompress: true,
-					optimization: 2
+		grunt.initConfig({
+				jshint: {
+					files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js', 'js/**/*.js'],
+					options: {
+						globals: {
+							jQuery: true
+						}
+					}
 				},
-				files: {
-					"css/app.css": "less/app.less"
-				}
-			}
-		}
-	});
+				watch: {
+					js: {
+						files: ['<%= jshint.files %>'],
+						tasks: ['jshint']
+						jshint: {},
+						styles: {
+							files: ['public/less/**/*.less'], // which files to watch
+							tasks: ['less'],
+							options: {
+								nospawn: true
+							}
+						}
+					},
+					less: {
+						development: {
+							options: {
+								compress: true,
+								yuicompress: true,
+								optimization: 2
+							},
+							files: {
+								"css/app.css": "less/app.less"
+							}
+						}
+					}
+				});
 
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-less');
+			grunt.loadNpmTasks('grunt-contrib-jshint'); grunt.loadNpmTasks('grunt-contrib-watch'); grunt.loadNpmTasks('grunt-contrib-less');
 
-	grunt.registerTask('default', ['jshint']);
+			grunt.registerTask('default', ['jshint']);
 
-};
+		};
