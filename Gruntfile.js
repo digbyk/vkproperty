f2e9d8fd29f73f422f66eb4b3f5f3301bae06d08
digbyk <digby@digby.net>
2015-04-25 09:36:45 +0100

    Refactoring menu

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,22 @@
 module.exports = function (grunt) {
+	require('jit-grunt')(grunt);
+
+	grunt.loadNpmTasks('grunt-git');
+	grunt.loadNpmTasks('grunt-mocha-test');
+
 	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		mochaTest: {
+			test: {
+				options: {
+					reporter: 'spec',
+					captureFile: 'results.txt', // Optionally capture the reporter output to a file
+					quiet: false, // Optionally suppress output to standard out (defaults to false)
+					clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
+				},
+				src: ['test/**/*.js']
+			}
+		},
 		less: {
 			development: {
 				options: {
@@ -8,7 +25,7 @@ module.exports = function (grunt) {
 					optimization: 2
 				},
 				files: {
-					"./public/css/main.css": ["./public/less/main.less"] // destination file and source file
+					"./public/css/main.css": ["./public/less/bootstrap.less", "./public/less/main.less"] // destination file and source file
 				}
 			}
 		},
@@ -20,9 +37,37 @@ module.exports = function (grunt) {
 					nospawn: true
 				}
 			}
+		},
+		gitpush: {
+			master: {
+				options: {
+					verbose: true,
+					branch: 'master',
+					remote: 'github'
+				}
+			},
+			staging: {
+				options: {
+					verbose: true,
+					branch: 'staging',
+					remote: 'github'
+				}
+			},
+			openshift: {
+				options: {
+					verbose: true,
+					branch: 'staging',
+					remote: 'openshift-tst'
+				}
+			}
 		}
 	});
 
+	grunt.registerTask('run-tests', ['mochaTest']);
+	grunt.registerTask('openshift-test-deploy', ['openshift_tst_deploy']);
+	grunt.registerTask('github-push-master', ['gitpush:master']);
+	grunt.registerTask('github-push-staging', ['gitpush:staging']);
+	grunt.registerTask('github-push-openshift', ['gitpush:openshift']);
+	grunt.registerTask('watch-less', ['watch']);
 	grunt.registerTask('compile-less', ['less']);
-
 };
