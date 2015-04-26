f822db332820433b7210baa4674a2420ef5b8f58
digbyk <digby@digby.net>
2015-04-26 23:32:55 +0100

    Adding test stub and sorting gitignore

---
+++ test/test.js
@@ -0,0 +1,10 @@
+var expect = require('expect.js');
+
+describe('Service', function () {
+	describe('#doSomething()', function () {
+		it('should return okay', function (done) {
+			expect(true).to.be.ok();
+			done();
+		})
+	});
+});
