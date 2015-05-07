3e92ad04be934b0716e50fac5a920264c1a2c102
digbyk <digby@digby.net>
2015-05-07 23:28:14 +0100

    Fixing tests for schools service

--- test/test.js
+++ test/test.js
@@ -1,10 +1,16 @@
 var expect = require('expect.js');
+require('../config/db.js');
+var schoolsService = require('../services/schools');
 
-describe('Service', function () {
-	describe('#doSomething()', function () {
-		it('should return okay', function (done) {
-			expect(true).to.be.ok();
-			done();
-		})
+describe('SchoolsService', function () {
+	describe('#getSchools()', function () {
+		it('should return a list of schools', function (done) {
+			schoolsService.getSchools(function (err, data) {
+				if (err) throw err;
+				expect(data).to.be.ok;
+				expect(data.length).to.be(251);
+				done();
+			});
+		});
 	});
 });
